99dd68ae88b2c80a1a9a91fca66f158ada57310b
Aaron <aaronespinozadr@gmail.com>
2019-04-12 19:26:37 -0500

    Previo

--- controllers/crudControlador.php
+++ controllers/crudControlador.php
@@ -5,7 +5,7 @@ require  '../models/crud.php';
 $list=new crud(); /*Si te das cuenta no se instancia la clase conexión, solo lo haces en tu misma clase crud, pero aún asi se pone la ruta de la conexión 
             para cuando tu clase crud busque la clase conexion al instanciarla la encuentre*/
 $res1= $list->Listar(); //llamas a tu función listar la cual te retornara el $res(los registros de tu consulta; de lo contrario retornara el mensaje de error)
-//print_r($res1);
+// print_r($res1);
 
 
 ?>
